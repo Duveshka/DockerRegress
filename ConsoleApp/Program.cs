6b312c100125c818de056ed989647325937fd75e
ekaterina.kuznetsova <ekaterina.kuznetsova@jetbrains.com>
2023-03-20 19:49:49 +0200

    Add project with Dockerfile

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿int r = 0;
+while(true)
+{
+    var dt = DateTime.Now;
+    r = new Random().Next();
+    Console.WriteLine($"{dt}: Hello, World! This is random number: {r}"); //TODO: Set a breakpoint
+    Thread.Sleep(5000);
+}
